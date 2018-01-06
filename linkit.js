5d4658a3bbf20d9e9f7067ad51f10a9362a42749
Dan Netherton <djnetherton@gmail.com>
2018-01-06 15:26:45 +0000

    Tidied the JS a bit and updated examples. Also added newWindow option

--- linkit.js
+++ linkit.js
@@ -1,27 +1,30 @@
-if (typeof jQuery == 'undefined'){
+if (typeof jQuery === 'undefined'){
     console.log('Link it requires jQuery to run');
 }else{
     (function ($) {
-    "use strict";
-    $.fn.linkIt = function (options) {
-        let settings = $.extend({
-            linkColor: "#ff0000",
-            linkClass: 'linkit-link',
-            linkWord: ['', '#', ''],
-            caseSensitive: false
-        }, options);
-
-        let $str = $(this);
-
-        $str.each(function (index, element) {
-            let strText = this.innerText,
+        "use strict";
+        $.fn.linkIt = function (options) {
+            let settings = $.extend({
+                linkColor: "#ff0000",
+                linkClass: 'linkit-link',
+                linkWord: ['', '#', ''],
+                newWindow: false,
+                caseSensitive: false
+            }, options);
+            
+            let $str = $(this);
+            
+            $str.each(function (index, element) {
+                let strHtml = $(this).html(),
                 query = "\\b" + settings.linkWord[0] + "\\b",
-                flags = settings.caseSensitive = true ? 'g' : 'gi',
+                flags = settings.caseSensitive === true ? 'g' : 'gi',
+                target = settings.newWindow === true ? ' target="_blank"' : '',
                 regex = new RegExp(query, flags),
-                result = strText.replace(regex, "<a href=" + settings.linkWord[1]+ " style='color:" + settings.linkColor + "' class='" + settings.linkClass + "'>" + settings.linkWord[0] + "</a>");
-                console.log(result);
-            return this.innerHTML = result;  
-        });
-    };
-}(jQuery));
+                newHtml = `<a href="${settings.linkWord[1]}" style="color:${settings.linkColor};" class="${settings.linkClass}" ${target}>${settings.linkWord[0]}</a>`,
+                result = strHtml.replace(regex, newHtml);
+                console.log(target);
+                return $(this).html(result);
+            });
+        };
+    }(jQuery));
 }
